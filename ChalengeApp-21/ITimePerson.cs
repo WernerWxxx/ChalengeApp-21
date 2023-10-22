6e3d81f0eea31d36cd6aaf73735308688271fb75
wernerwxxx7547 <tsp1rejestr@vp.pl>
2023-10-22 19:12:55 +0200

    Test 2

---
+++ ChalengeApp-21/ITimePerson.cs
@@ -0,0 +1,27 @@
+﻿using static ChalengeApp.TimePersonBase;
+
+namespace ChalengeApp
+{
+    public interface ITimePerson
+    {
+        string Name { get; }
+
+        string Surname { get; }
+
+        //int Age { get; }//
+
+        void AddGrade(float grade);
+
+        void AddGrade(double grade);
+
+        void AddGrade(long grade);
+
+        void AddGrade(int grade);
+
+        void AddGrade(string grade);
+
+        event GradeAddedDelegate GradeAdded;
+        Statistics GetStatistics();
+
+    }
+}
---
+++ ChalengeApp-21/TimePersonBase.cs
@@ -0,0 +1,29 @@
+﻿namespace ChalengeApp
+{
+    public abstract class TimePersonBase : ITimePerson
+    {
+        public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
+        public abstract event GradeAddedDelegate GradeAdded;
+        public TimePersonBase(string name, string surname)//
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public abstract void AddGrade(float grade);
+
+        public abstract void AddGrade(double grade);
+
+        public abstract void AddGrade(long grade);
+
+        public abstract void AddGrade(int grade);
+
+        public abstract void AddGrade(string grade);
+
+        public abstract Statistics GetStatistics();
+    }
+}
