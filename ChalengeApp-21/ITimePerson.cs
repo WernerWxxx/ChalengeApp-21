59baacefb28fb0745467d745405fa9ccd12549d1
wernerwxxx7547 <tsp1rejestr@vp.pl>
2023-10-27 16:17:09 +0200

    Zadanie Domowe Dzień 21 b - do Certyfikacji

---
+++ ChalengeApp-21/ITimePerson.cs
@@ -0,0 +1,29 @@
+﻿using static ChalengeApp_21.TimePersonBase;
+
+namespace ChalengeApp_21
+{
+    public interface ITimePerson
+    {
+        string Name { get; }
+
+        string Surname { get; }
+
+        //int Age { get; }//
+
+        void AddGrade(float grade);
+
+        void AddGrade(double grade);
+
+        void AddGrade(long grade);
+
+        void AddGrade(int grade);
+
+        //void AddGrade(char grade);
+
+        void AddGrade(string grade);
+
+        event GradeAddedDelegate GradeAdded;
+        Statistics GetStatistics();
+
+    }
+}
